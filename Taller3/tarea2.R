0966749563318670b1426ce9704affeec49c298c
Valee25 <valery.lizarazo@urosario.edu.co>
2026-08-12 14:53:02 -0500

    Update tarea2.R

--- Taller3/tarea2.R
+++ Taller3/tarea2.R
@@ -1,46 +1,16 @@
-tenderos_tarea2 <- bind_rows(
-  TenderosFU03_Publica %>%
-    filter(actG1==1) %>%
-    summarise(actividad="actG1", internet=mean(uso_internet)*100),
+library(dplyr)
+library(tidyr)
+library(haven)
+
+tenderos_tarea1=TenderosFU03_Publica %>%
+  select(uso_internet, actG1:actG11)
   
-  TenderosFU03_Publica %>%
-    filter(actG2==1) %>%
-    summarise(actividad="actG2", internet=mean(uso_internet)*100),
+tarea2_ <- tenderos_tarea1 %>% 
+ 
+  pivot_longer(
+                cols = actG1:actG11, names_to = "actividad", values_to = "ok"
   
-  TenderosFU03_Publica %>%
-    filter(actG3==1) %>%
-    summarise(actividad="actG3", internet=mean(uso_internet)*100),
-  
-  TenderosFU03_Publica %>%
-    filter(actG4==1) %>%
-    summarise(actividad="actG4", internet=mean(uso_internet)*100),
-  
-  TenderosFU03_Publica %>%
-    filter(actG5==1) %>%
-    summarise(actividad="actG5", internet=mean(uso_internet)*100),
-  
-  TenderosFU03_Publica %>%
-    filter(actG6==1) %>%
-    summarise(actividad="actG6", internet=mean(uso_internet)*100),
-  
-  TenderosFU03_Publica %>%
-    filter(actG7==1) %>%
-    summarise(actividad="actG7", internet=mean(uso_internet)*100),
-  
-  TenderosFU03_Publica %>%
-    filter(actG8==1) %>%
-    summarise(actividad="actG8", internet=mean(uso_internet)*100),
-  
-  TenderosFU03_Publica %>%
-    filter(actG9==1) %>%
-    summarise(actividad="actG9", internet=mean(uso_internet)*100),
-  
-  TenderosFU03_Publica %>%
-    filter(actG10==1) %>%
-    summarise(actividad="actG10", internet=mean(uso_internet)*100),
-  
-  TenderosFU03_Publica %>%
-    filter(actG11==1) %>%
-    summarise(actividad="actG11", internet=mean(uso_internet)*100),
-)
-View(tenderos_tarea2)
+  )%>%
+filter(ok == 1) 
+
+group_by(tarea2_)
